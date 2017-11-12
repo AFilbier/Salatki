b662159d28eb056a789a7dd09a96801995c4ac63
Artur <velius1@wp.pl>
2017-11-12 17:27:24 +0100

    Jsons are now list of dictionaries, separate files for different food groups

--- main.py
+++ main.py
@@ -5,45 +5,68 @@ import os
 
 
 #inicjalizacja pustych tupli do listowania skladnikow
+dict_bazy = []
+dict_dodatki = []
+dict_sosy = []
+
 bazy = []
 dodatki = []
 sosy = []
 
-#Produkt = skladnik("nazwa",bialko per 100g, tluszcz per 100g, wegle per 100g, typowa masa w opakowaniu, kcal per 100g, typ = baza, warzywo, bialko, sos)
+# Typy: baza, mieso, warzywo, sos
 
-# Kurczak = skladnik("Kurczak",23, 20, 40, 200, 500, "bialko")
-# Tunczyk = skladnik("Mieso",23, 20, 40, 200, 500, "bialko")
-# Salata = skladnik("Salata", 10,10,40, 300,100, "baza")
-# Kukurydza = dodatek("Kukurydza", 10,10,60,200,400,"warzywo","slodka")
-# papryka = dodatek("papryka",20,10,10,300,200,"warzywo","duzo wit c")
-# ocet = sos("ocet", 5,0,0,100,50,"kwasny")
-
-
-slownik = {}
-plik = open("./skladniki.json", "r")
-slownik = (json.loads(plik.readline()))
-
-
-#Typy: baza, mieso, warzywo, sos
-
-if slownik['typ'] == "bialko":
-    obiekt = skladnik(slownik['nazwa'], slownik['protein'], slownik['fat'], slownik['carb'], slownik['masa'], slownik['kcal'], slownik['typ'])
-
-if obiekt.getTyp() == "bialko" or obiekt.getTyp == "warzywo":
-    dodatki.append(obiekt.getName())
-elif obiekt.getTyp() == "baza":
-    bazy.append(obiekt.getName())
-elif obiekt.getTyp() == "sos":
-    sosy.append(obiekt.getName())
-else:
-    print("Nie rozpoznano typu: ", obiekt.getTyp(), "skladnika ", obiekt.getName())
-
-
+plik = open("./bazy.json", "r")
+dict_bazy = json.loads(plik.read())
 plik.close()
 
+for i in dict_bazy:
+     if i['typ'] == "baza":
+         obiekt = skladnik(i['nazwa'], i['protein'], i['fat'], i['carb'], i['masa'], i['kcal'], i['typ'])
+         bazy.append(obiekt)
 
 
-#cechy z dodatkow wrzuc w jeden set zeby usunac duplikaty
+
+
+
+
+
+
+
+
+
+
+
+
+# if slownik['typ'] == "bazy":
+#     obiekt = skladnik(slownik['nazwa'], slownik['protein'], slownik['fat'], slownik['carb'], slownik['masa'], slownik['kcal'], slownik['typ'])
+# if obiekt.getTyp() == "bialko" or obiekt.getTyp == "warzywo":
+#     dodatki.append(obiekt.getName())
+# elif obiekt.getTyp() == "baza":
+#     bazy.append(obiekt.getName())
+# elif obiekt.getTyp() == "sos":
+#     sosy.append(obiekt.getName())
+# else:
+#     print("Nie rozpoznano typu: ", obiekt.getTyp(), "skladnika ", obiekt.getName())
+# print(slownik)
+
+
+
+
+
+
+         # Produkt = skladnik("nazwa",bialko per 100g, tluszcz per 100g, wegle per 100g, typowa masa w opakowaniu, kcal per 100g, typ = baza, warzywo, bialko, sos)
+
+         # Kurczak = skladnik("Kurczak",23, 20, 40, 200, 500, "bialko")
+         # Tunczyk = skladnik("Mieso",23, 20, 40, 200, 500, "bialko")
+         # Salata = skladnik("Salata", 10,10,40, 300,100, "baza")
+         # Kukurydza = dodatek("Kukurydza", 10,10,60,200,400,"warzywo","slodka")
+         # papryka = dodatek("papryka",20,10,10,300,200,"warzywo","duzo wit c")
+         # ocet = sos("ocet", 5,0,0,100,50,"kwasny")
+
+         #cechy z dodatkow wrzuc w jeden set zeby usunac duplikaty
+
+
+
 
 #wrzucaj kolejne skladniki do listy? slownika?
 
@@ -55,10 +78,17 @@ plik.close()
 
 #funkcja zapisujaca przepis do pliku
 
+#plik = open("./skladniki.json", "w+")
 
-#Testowy zapis 1 obiektu do jsona
+# #Testowy zapis 1 obiektu do jsona
 # slownik = {"nazwa": Kurczak.getName(), "protein": Kurczak.getProt(), "fat": Kurczak.getFat(), "carb": Kurczak.getCarb(), "masa": Kurczak.getMasa(), "kcal": Kurczak.getKcal(),"typ": Kurczak.getTyp()}
+# slownik2 = {"nazwa": Tunczyk.getName(), "protein": Tunczyk.getProt(), "fat": Tunczyk.getFat(), "carb": Tunczyk.getCarb(), "masa": Tunczyk.getMasa(), "kcal": Tunczyk.getKcal(),"typ": Tunczyk.getTyp()}
+# slownik3 = {"nazwa": Tunczyk.getName(), "protein": Tunczyk.getProt(), "fat": Tunczyk.getFat(), "carb": Tunczyk.getCarb(), "masa": Tunczyk.getMasa(), "kcal": Tunczyk.getKcal(),"typ": Tunczyk.getTyp()}
 #
-# plik = open("./skladniki.json", "r+")
-# plik.write(json.dumps(slownik))
+# dodatki.append(slownik)
+# dodatki.append(slownik2)
+# dodatki.append(slownik3)
+#
+# #print(dodatki)
+# plik.write(json.dumps(dodatki))
 # plik.close()
